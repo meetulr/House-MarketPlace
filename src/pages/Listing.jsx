b8f3d51320e6a2cc9f7137178845c46e30a938e8
meetul <meetulrathore98@gmail.com>
2023-02-18 15:42:35 +0530

    add swiper

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,15 +2,15 @@ import { useState, useEffect } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 // import { Helmet } from 'react-helmet'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-// import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
-// import { Swiper, SwiperSlide } from 'swiper/react'
-// import 'swiper/swiper-bundle.css'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firsbase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
-// SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
     const [listing, setListing] = useState(null)
@@ -43,8 +43,12 @@ function Listing() {
         <main>
             {/* <Helmet>
                 <title>{listing.name}</title>
-            </Helmet>
-            <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+            </Helmet> */}
+            <Swiper
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+                style={{ height: 350 }}
+            >
                 {listing.imageUrls.map((url, index) => (
                     <SwiperSlide key={index}>
                         <div
@@ -56,7 +60,7 @@ function Listing() {
                         ></div>
                     </SwiperSlide>
                 ))}
-            </Swiper> */}
+            </Swiper>
 
             <div
                 className='shareIconDiv'
