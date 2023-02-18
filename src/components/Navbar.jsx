8bdb0db70ec0e9d0478b5953002027193f4db174
meetul <meetulrathore98@gmail.com>
2023-02-18 14:23:10 +0530

    get and create listings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
                     </li>
                     <li className="navbarListItem" onClick={() => navigate("/offers")} >
                         <ExploreIcon fill={pathMatchRoute("/offers") ? "#2c2c2c" : "#8f8f8f"} width="36px" height="36px" />
-                        <p className={pathMatchRoute("/offers") ? "navbarListItemNameActive" : "navbarListItemName"}>Offer</p>
+                        <p className={pathMatchRoute("/offers") ? "navbarListItemNameActive" : "navbarListItemName"}>Offers</p>
                     </li>
                     <li className="navbarListItem" onClick={() => navigate("/profile")} >
                         <ExploreIcon fill={pathMatchRoute("/profile") ? "#2c2c2c" : "#8f8f8f"} width="36px" height="36px" />
